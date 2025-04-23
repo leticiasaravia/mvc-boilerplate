bbf6ab3d4c3fcc82ebfde575dc6c3da8d819fb9c
Leticia Saravia <leticia.saravia@sou.inteli.edu.br>
2025-04-23 11:50:41 -0300

    First commit

---
+++ controllers/aboutController.js
@@ -0,0 +1,8 @@
+// Controlador da rota /sobre
+exports.index = (req, res) => {
+    res.render('pages/about', {
+      titulo: 'Sobre',
+      descricao: 'Esta é a página sobre do mini site MVC.'
+    });
+  };
+  
